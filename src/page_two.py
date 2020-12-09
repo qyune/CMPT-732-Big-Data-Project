4d7d9b254aa0fe61a2555e4793ed0b681906094c
Kyoun Huh <qyune.h@gmail.com>
2020-12-09 15:42:56 +0900

    edited page_two.py

--- src/page_two.py
+++ src/page_two.py
@@ -23,17 +23,19 @@ observation_schema = types.StructType([
 
 @functions.udf(returnType=types.StringType())
 def country_convert(country):
+    """Converting a country name to country code (ISO-3)"""
     return coco.convert(names=country, to='iso3')
 
-
+# Connecting to AWS RDS (Postgresql Engine) - output
 engine = create_engine(
     'postgresql+psycopg2://postgres:Aws_2020@database-1.cwfbooless1u.us-east-1.rds.amazonaws.com:5432/postgres')
 
 
 def main():
+    # Reads input source from AWS S3 bucket
     temperature_df = spark.read.format('csv').options(header='true', schema='observation_schema') \
         .option("mode", "DROPMALFORMED").load("s3://climate-data-732/GlobalLandTemperaturesByCity.csv")
-        # .option("mode", "DROPMALFORMED").load("GlobalLandTemperaturesByCity.csv")
+    # .option("mode", "DROPMALFORMED").load("GlobalLandTemperaturesByCity.csv")
 
     temperature_df.createOrReplaceTempView("temperature_view")
     temperature_df = spark.sql("SELECT dt, AverageTemperature, City, Country "
@@ -57,7 +59,7 @@ def main():
         functions.avg('AverageTemperature').alias('Avg_Temp_World')).orderBy('Year')
     avg_temp_df_world = avg_temp_df_world.withColumn('Avg_Temp_World',
                                                      functions.round(avg_temp_df_world['Avg_Temp_World'], 2))
-    avg_temp_df_world.show(30)
+
     # Save to DB P2-2
     avg_temp_df_world.toPandas().to_sql('graph_2_2', engine, if_exists='replace')
 
@@ -84,20 +86,22 @@ def main():
 
     diff_df = joined_df.withColumn('Diff', (joined_df['Avg_12'] - joined_df['Avg_85'])).orderBy('Country')
 
+    # Adding country code column
     code_df = diff_df.withColumn('Code', country_convert(diff_df['Country']))
 
+    # adjust floating point precision to 2
     res_df = code_df.withColumn('Avg_85', functions.round(code_df['Avg_85'], 2))
     res_df = res_df.withColumn('Avg_12', functions.round(code_df['Avg_12'], 2))
     res_df = res_df.withColumn('Diff', functions.round(code_df['Diff'], 2))
-    res_df.show()
+
     # Save to DB P2-1
     res_df.toPandas().to_sql('graph_2_1', engine, if_exists='replace')
 
-
     rank_df = res_df.orderBy('Diff', ascending=False)
-    rank_df.show()
+
     # Save to DB P2-3
     rank_df.toPandas().to_sql('graph_2_3', engine, if_exists='replace')
+
 
 if __name__ == '__main__':
     spark = SparkSession.builder.appName('correlate logs').getOrCreate()
